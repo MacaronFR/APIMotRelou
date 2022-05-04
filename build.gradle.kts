a79d71bb10de81fc10e453101c3276664ed3b3b7
MacaronFR <denisft77@gmail.com>
2022-05-04 21:37:38 +0200

    ktor init

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,30 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
 
 plugins {
+    application
     kotlin("jvm") version "1.6.20"
 }
 
 group = "fr.imacaron"
-version = "1.0-SNAPSHOT"
+version = "2.0"
+
+application {
+    mainClass.set("fr.imacaron.ApplicationKt")
+
+    val isDevelopment: Boolean = project.ext.has("developement")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.developmebt=$isDevelopment")
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
