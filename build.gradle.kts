fe16f88ee94e05b1c4b8656059d21e734d7a7014
Denis Turbiez <denisft77@gmail.com>
2022-10-19 18:32:27 +0200

    shadow jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
 	kotlin("jvm") version "1.6.21"
 	kotlin("plugin.serialization") version "1.6.21"
 	id("org.jetbrains.dokka") version "1.6.21"
+	id("com.github.johnrengelman.shadow") version "7.0.0"
 }
 
 group = "fr.imacaron"
@@ -36,6 +37,26 @@ dependencies {
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
 
+java{
+	manifest{
+		attributes(mapOf("Main-Class" to "fr.imacaron.motrelou.ApplicationKt"))
+	}
+}
+
+tasks.withType<Jar>{
+	manifest{
+		attributes["Main-Class"] = "fr.imacaron.motrelou.ApplicationKt"
+	}
+}
+
 tasks.dokkaHtml.configure{
 	outputDirectory.set(File("${buildDir.parentFile.absolutePath}/docs"))
+}
+
+tasks{
+	shadowJar{
+		manifest{
+			attributes("Main-Class" to "fr.imacaron.motrelou.ApplicationKt")
+		}
+	}
 }
