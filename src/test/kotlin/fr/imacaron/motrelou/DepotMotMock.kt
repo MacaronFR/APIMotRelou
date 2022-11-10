6cf9c67da2b492489fc7411775e503f86cc9047f
Denis Turbiez <denisft77@gmail.com>
2022-11-10 14:53:16 +0100

    unit test on service mot test

--- src/test/kotlin/fr/imacaron/motrelou/DepotMotMock.kt
+++ src/test/kotlin/fr/imacaron/motrelou/DepotMotMock.kt
@@ -1,6 +1,7 @@
 package fr.imacaron.motrelou
 
 import fr.imacaron.motrelou.depot.DepotMot
+import fr.imacaron.motrelou.depot.ExceptionConflitDepot
 import fr.imacaron.motrelou.depot.ExceptionMotIntrouvableDepot
 import fr.imacaron.motrelou.type.TDefinition
 import fr.imacaron.motrelou.type.TMajMot
@@ -10,7 +11,7 @@ import java.time.LocalDateTime
 
 class DepotMotMock: DepotMot{
 	companion object {
-		val liste = listOf(
+		val liste = mutableListOf(
 			TMot("Exact", "Denis", LocalDateTime.now(), mutableListOf(TDefinition("Définition", "Denis", LocalDateTime.now(), 1))),
 			TMot("Partiel1", "Denis", LocalDateTime.now(), mutableListOf(TDefinition("Définition", "Denis", LocalDateTime.now(), 1))),
 			TMot("Partiel2", "Denis", LocalDateTime.now(), mutableListOf(TDefinition("Définition", "Denis", LocalDateTime.now(), 1))),
@@ -18,41 +19,50 @@ class DepotMotMock: DepotMot{
 			TMot("Partiel4", "Denis", LocalDateTime.now(), mutableListOf(TDefinition("Définition", "Denis", LocalDateTime.now(), 1))),
 			TMot("1Partiel", "Denis", LocalDateTime.now(), mutableListOf(TDefinition("Définition", "Denis", LocalDateTime.now(), 1)))
 		)
+
+		val date = LocalDateTime.of(2022, 5, 8, 14, 45, 58)
 	}
 
-	override fun recherche(demande: String, limit: Int, page: Int): List<TMot> {
-		val search = if(limit >= liste.size){
-			liste
-		}else if(limit * page >= liste.size){
-			emptyList<TMot>()
-		}else if(limit * (page+1) >= liste.size){
-			liste.subList(limit * page, liste.size - 1)
-		}else{
-			liste.subList(limit*page, limit*(page+1))
-		}
-		return search.filter { it.mot.contains(demande) }
-	}
+	override fun recherche(demande: String, limit: Int, page: Int): List<TMot> = recuperer(limit, page).filter { demande in it.mot }
 
 	override fun recuperer(mot: String): TMot = liste.find { it.mot == mot } ?: throw ExceptionMotIntrouvableDepot()
 
-	override fun recuperer(limit: Int, page: Int): List<TMot> {
-		TODO("Not yet implemented")
-	}
+	override fun recuperer(limit: Int, page: Int): List<TMot> =
+		if(limit >= liste.size){
+			liste
+		}else if(limit * page >= liste.size){
+			emptyList()
+		}else if(limit * (page+1) >= liste.size){
+			liste.subList(limit * page, liste.size - 1)
+		}else {
+			liste.subList(limit * page, limit * (page + 1))
+		}
 
-	override fun modifier(mot: String, maj: TMajMot): TMot {
-		TODO("Not yet implemented")
-	}
+	override fun modifier(mot: String, maj: TMajMot): TMot = liste.find { it.mot == mot }?.apply {
+		maj.createur?.let {
+			createur = it
+		}
+	} ?: throw ExceptionMotIntrouvableDepot()
 
 	override fun supprimer(mot: String) {
-		TODO("Not yet implemented")
+		if(!liste.removeIf { it.mot == mot }){
+			throw ExceptionMotIntrouvableDepot()
+		}
 	}
 
-	override fun aleatoire(): TMot {
-		TODO("Not yet implemented")
-	}
+	override fun aleatoire(): TMot = liste.random()
 
 	override fun ajouter(mot: TNouveauMot): TMot {
-		TODO("Not yet implemented")
+		try {
+			recuperer(mot.mot)
+			throw ExceptionConflitDepot("Le mot existe déjà")
+		}catch(e: ExceptionMotIntrouvableDepot){
+			return TMot(
+				mot.mot,
+				mot.createur,
+				date,
+				mutableListOf(TDefinition(mot.definition, mot.createur, date, 1))
+			).apply { liste.add(this) }
+		}
 	}
-
 }
--- src/test/kotlin/fr/imacaron/motrelou/ServiceMotTest.kt
+++ src/test/kotlin/fr/imacaron/motrelou/ServiceMotTest.kt
@@ -1,14 +1,19 @@
 package fr.imacaron.motrelou
 
-import fr.imacaron.motrelou.depot.ExceptionMotIntrouvableDepot
 import fr.imacaron.motrelou.domaine.ServiceMot
+import fr.imacaron.motrelou.requete.ExceptionConflit
+import fr.imacaron.motrelou.requete.ExceptionMotIntrouvable
+import fr.imacaron.motrelou.type.TMajMot
+import fr.imacaron.motrelou.type.TNouveauMot
 import org.junit.jupiter.api.assertThrows
 import org.slf4j.LoggerFactory
 import kotlin.test.Test
+import kotlin.test.assertContains
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
 
 class ServiceMotTest {
-	val service = ServiceMot(DepotMotMock(), LoggerFactory.getLogger("Test"))
+	private val service = ServiceMot(DepotMotMock(), LoggerFactory.getLogger("Test"))
 
 	@Test
 	fun rechercheNoMatch(){
@@ -22,7 +27,12 @@ class ServiceMotTest {
 
 	@Test
 	fun rechercheMatchPartiel(){
-		assertEquals(DepotMotMock.liste.filter { it.mot != "Exact" }, service.recherche("Partiel"))
+		assertEquals(DepotMotMock.liste.filter { "Partiel" in it.mot }, service.recherche("Partiel"))
+	}
+
+	@Test
+	fun rechercheEmpty(){
+		assertEquals(DepotMotMock.liste, service.recherche(null))
 	}
 
 	@Test
@@ -32,8 +42,55 @@ class ServiceMotTest {
 
 	@Test
 	fun recupererInvalid(){
-		assertThrows<ExceptionMotIntrouvableDepot>{
+		assertThrows<ExceptionMotIntrouvable>{
 			service.recuperer("Invalid")
+		}
+	}
+
+	@Test
+	fun aleatoire(){
+		assert(service.aleatoire() in DepotMotMock.liste)
+	}
+
+	@Test
+	fun creer() {
+		val newMot = service.creer(TNouveauMot("Add", "Denis", "Add"))
+		assert(newMot in DepotMotMock.liste)
+	}
+
+	@Test
+	fun creerConflit(){
+		assertThrows<ExceptionConflit>{
+			service.creer(TNouveauMot("Exact", "Denis", "Exact"))
+		}
+	}
+
+	@Test
+	fun modifier(){
+		val motMaj = service.maj("Exact", TMajMot(createur = "Moi"))
+		assertContains(DepotMotMock.liste, motMaj)
+		assertEquals("Moi", motMaj.createur)
+	}
+
+	@Test
+	fun modifierIntrouvable(){
+		assertThrows<ExceptionMotIntrouvable> {
+			service.maj("Introuvable", TMajMot())
+		}
+	}
+
+	@Test
+	fun supprimer(){
+		val mot = service.creer(TNouveauMot("Add", "Denis", "Add"))
+		assertContains(DepotMotMock.liste, mot)
+		service.supprimer("Add")
+		assertFalse(mot in DepotMotMock.liste)
+	}
+
+	@Test
+	fun supprimerIntrouvable(){
+		assertThrows<ExceptionMotIntrouvable> {
+			service.supprimer("Inexact")
 		}
 	}
 }
